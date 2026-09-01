bb755e10d59adc7711e2aa8b5ce704a1a833da97
surya-4510 <sksahoo4510@gmail.com>
2026-09-01 15:02:11 +0530

    Create asgi.py

---
+++ team_todo/asgi.py
@@ -0,0 +1,11 @@
+"""
+ASGI config for team_todo project.
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'team_todo.settings')
+
+application = get_asgi_application()
